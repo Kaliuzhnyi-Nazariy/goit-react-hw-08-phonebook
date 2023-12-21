101670f490c7eab4da53d54f49398cbe10e60801
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2023-12-21 18:42:21 +0000

    Deploying to gh-pages from @ Kaliuzhnyi-Nazariy/goit-react-hw-08-phonebook@9f5ae771fa6cce1002b708b626666bb43256c384 🚀

---
+++ static/js/922.0cf6d97f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[922],{8922:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var r=t(184);function c(){return(0,r.jsxs)("div",{style:{display:"flex",alignItems:"center",flexDirection:"column",marginTop:"20%"},children:[(0,r.jsx)("h2",{children:"Something went wrong!\ud83d\ude14"}),(0,r.jsx)("p",{children:"Please, back to home\ud83d\ude09"})]})}}}]);
+//# sourceMappingURL=922.0cf6d97f.chunk.js.map
